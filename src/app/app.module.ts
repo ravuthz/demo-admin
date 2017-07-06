0afb9ac8701252158d896a109a7046413178c5af
Ravuthz <v.yo@gl-f.com>
2017-07-06 16:53:32 +0700

    Add App Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+
+//import { routing } from './app.routing';
+//import { AppRouteModule } from './app.route';
+
+const routes:Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent }
+];
+
 
 @NgModule({
     declarations: [
@@ -14,10 +25,15 @@ import { HomeComponent } from './home/home.component';
     imports: [
         BrowserModule,
         FormsModule,
-        HttpModule
+        HttpModule,
+
+        //routing
+        //AppRouteModule
+        RouterModule.forRoot(routes)
     ],
     providers: [],
     bootstrap: [AppComponent]
 })
+
 export class AppModule {
 }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders }  from '@angular/core';
+
+import { HomeComponent } from './home/home.component';
+
+export const routes:Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'work', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent }
+];
+
+export const routing:ModuleWithProviders = RouterModule.forRoot(routes);
